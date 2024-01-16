a7f82b82816c1f7a369d9c74dd0388315219a881
Casqabel <cascabeltb@gmail.com>
2024-01-16 16:15:50 +0100

    Script de camara
    Programada mainCamera para seguimiento suave

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+
+    
+{
+
+    [SerializeField]
+    Transform player;
+   
+    Vector3 offset;
+    [SerializeField]
+    float smoothTargetTime;
+
+    Vector3 smoothDampVelocity;
+
+    
+
+    private void Awake()
+    {
+        offset = this.transform.position - player.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, player.position + offset,
+           ref smoothDampVelocity, smoothTargetTime);
+    }
+}
+    
+
