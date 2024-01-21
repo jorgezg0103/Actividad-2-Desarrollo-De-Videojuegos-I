208b77ae2ee9872941b004f977fb3c8ef322c0f0
Raquel Garrido <raquela82@gmail.com>
2024-01-21 23:44:03 +0100

    Add snake enemy

---
+++ Assets/Scripts/SnakeController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour
+{
+    Vector3 startingPos;
+    Transform trans;
+
+    void Start() {
+        trans = GetComponent<Transform>();
+        startingPos = trans.position;
+    }
+
+    void Update() {
+        trans.position = new Vector3(startingPos.x, startingPos.y + Mathf.PingPong(Time.time, 3), startingPos.z);
+    }
+}
